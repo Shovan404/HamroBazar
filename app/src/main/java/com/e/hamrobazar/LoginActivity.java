278f8a3b8a4600907831618ca9145b75d92ea4e7
Shovan404 <shovanrj4@gmail.com>
2020-01-05 02:47:51 -0500

    Half design and binding

--- app/src/main/java/com/e/hamrobazar/LoginActivity.java
+++ app/src/main/java/com/e/hamrobazar/LoginActivity.java
@@ -3,12 +3,29 @@ package com.e.hamrobazar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister, btnForgot;
+    private EditText etEmail, etPassword;
+    private TextView tvRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //binding
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        tvRegister=findViewById(R.id.tvRegister);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnForgot=findViewById(R.id.btnForgot);
+        btnRegister=findViewById(R.id.btnRegister);
+
     }
 }
--- app/src/main/java/com/e/hamrobazar/SignUpActivity.java
+++ app/src/main/java/com/e/hamrobazar/SignUpActivity.java
@@ -3,12 +3,39 @@ package com.e.hamrobazar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    CheckBox cb1, cb2, cb3;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //binding
+        cb1=findViewById(R.id.cb1);
+        cb2=findViewById(R.id.cb2);
+        cb3=findViewById(R.id.cb3);
+
+
+
+
+
+
+
+        cb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cb1.isChecked()){
+
+                }
+            }
+        });
+
     }
 }
